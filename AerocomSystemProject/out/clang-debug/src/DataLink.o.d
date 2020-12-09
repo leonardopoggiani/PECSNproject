013d8cc688823f7b15b0fdfdb8d18e22dae3d969
Leonardo Poggiani <37518886+Poggio98@users.noreply.github.com>
2020-12-09 14:19:57 +0100

    Cancellato progetto doppione

--- AerocomSystemProject/out/clang-debug/src/DataLink.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-debug/src/DataLink.o: DataLink.cc DataLink.h \
-  C:/omnetpp-5.5.1/include\omnetpp.h
-
-DataLink.h:
-
-C:/omnetpp-5.5.1/include\omnetpp.h:
--- AerocomSystemProject/out/gcc-release/src/Aircraft.o.d
+++
@@ -1,6 +0,0 @@
-../out/gcc-release/src/Aircraft.o: Aircraft.cc Aircraft.h \
- /home/clarissa/omnetpp-5.6.2/include/omnetpp.h
-
-Aircraft.h:
-
-/home/clarissa/omnetpp-5.6.2/include/omnetpp.h:
