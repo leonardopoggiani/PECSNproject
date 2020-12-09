03d09cacaacbb00764c1e5d87e19e48137aa263b
Leonardo Poggiani <37518886+Poggio98@users.noreply.github.com>
2020-12-09 21:25:33 +0100

    Modifiche a Link Selector
    Scritte solamente le firme delle funzioni

--- src/LinkSelector.h
+++ src/LinkSelector.h
@@ -1,22 +1,23 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+#ifndef __AEROCOMSYSTEMPROJECT_DATALINK_H_
+#define __AEROCOMSYSTEMPROJECT_DATALINK_H_
 
-#ifndef LINKSELECTOR_H_
-#define LINKSELECTOR_H_
+#include <omnetpp.h>
 
-class LinkSelector {
+using namespace omnetpp;
+
+/**
+ * TODO - Generated class
+ */
+class LinkSelector : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+  private:
+    void handlePacketArrival(AircraftPacket* ap);
+    int getCapacity();
+    void handleSetCapacity(cMessage* msg);
+    void scheduleCheckCapacity();
 };
 
-#endif /* LINKSELECTOR_H_ */
+#endif
